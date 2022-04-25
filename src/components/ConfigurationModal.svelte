029188066ebb0f27393e1fa983d89c298db72080
Mohammed-Ali Hussein <mohammed-ali-hussein@mohammedalismbp.home>
2022-04-26 01:33:51 +0800

    Finished styling the config modal. Can now start on dispatching payload to parent

--- src/components/ConfigurationModal.svelte
+++ src/components/ConfigurationModal.svelte
@@ -16,6 +16,17 @@
         <!-- svelte-ignore a11y-missing-attribute -->
         <svg on:click xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 172 172" style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="10" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M26.5525,21.6075l-4.945,4.945l59.4475,59.4475l-59.4475,59.4475l4.945,4.945l59.4475,-59.4475l59.4475,59.4475l4.945,-4.945l-59.4475,-59.4475l59.4475,-59.4475l-4.945,-4.945l-59.4475,59.4475z"></path></g></g></svg>
         <h3>Scheduler Configuration</h3>
+        <div class="algorithms">
+            <h4>Algorithms</h4>
+            <div class="algorithm-types">
+                <h5>FCFS</h5>
+                <h5>SSTF</h5>
+                <h5>SCAN</h5>
+                <h5>C-SCAN</h5>
+                <h5>LOOK</h5>
+                <h5>C-LOOK</h5>
+            </div>
+        </div>
         <div class="head-direction">
             <h4>Head Direction</h4>
             <div class="head-direction-options">
@@ -23,7 +34,7 @@
                 <h5>Right</h5>
             </div>
         </div>
-        <input class="cylinders-input" type="text" placeholder="Cylinders">
+        <input class="cylinders-input" type="text" placeholder="Number of Cylinders">
         <input class="requests-input" type="text" placeholder="Disk Requests">
         <button>Confirm Config</button>
     </div>
@@ -52,7 +63,7 @@
         justify-content: center;
         align-items: center;
         flex-direction: column;
-        background-color: rgb(12, 12, 12);
+        background-color: rgb(8, 8, 8);
         animation: fadeIn 0.2s ease-in-out;
         margin: 20px;
     }
@@ -70,6 +81,35 @@
         cursor: pointer;
     }
 
+    .algorithms {
+        width: 80%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+        background:none;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    .algorithms h4 {
+        margin-bottom: 10px;
+    }
+
+    .algorithm-types {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        background: none;
+    }
+
+    .algorithm-types h5 {
+        margin: 3px;
+    }
+
     .head-direction, h4, h5, .head-direction-options {
         background: none;
         color: white;
@@ -77,7 +117,6 @@
 
     .head-direction {
         margin-bottom: 20px;
-        margin-top: 30px;
     }
 
     .head-direction-options {
@@ -92,6 +131,15 @@
         font-size: 28px;
     }
 
+    h5 {
+        margin-top: 5px;
+        font-weight: 300;
+        border: 1px solid rgba(255, 255, 255, 0.465);
+        padding: 2px 7px;
+        cursor: pointer;
+        transition: cubic-bezier(0.075, 0.82, 0.165, 1.0) 0.25s;
+    }
+
     .cylinders-input {
         margin-bottom: 20px;
     }
@@ -101,7 +149,7 @@
         border: none;
         outline: none;
         color: white;
-        padding: 5px 10px;
+        padding: 5px 8px;
         border: 1px solid rgba(255, 255, 255, 0.089);
     }
 
@@ -110,7 +158,7 @@
         color: white;
         border: 1px solid white;
         padding: 3px 7px;
-        margin-top: 30px;
+        margin-top: 20px;
         font-weight: 100;
         font-size: 13px;
         cursor: pointer;
@@ -118,7 +166,7 @@
         animation: translate-down cubic-bezier(0.075, 0.82, 0.165, 1.0) 1s;
     }
 
-    button:hover {
+    button:hover, h5:hover {
         background-color: white;
         color: black;
     }
